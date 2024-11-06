e639cf729defd9e88ed587ce78fdf9c6a4fc2d6d
rahul208in <rahul208in@gmail.com>
2024-11-06 19:32:48 +0800

    Update List api.js

--- DinAga/List api.js
+++ DinAga/List api.js
@@ -4,6 +4,12 @@ import path from 'path';
 
 export default function handler() {
   const uploadDir = path.join(process.cwd(), 'public/uploads');
-  const files = fs.readdirSync(uploadDir);
+  
+  // Ensure the directory exists
+  if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true });
+  }
+
+  const files = fs.readdirSync(uploadDir) || [];
   return new Response(JSON.stringify(files), { status: 200 });
 }
