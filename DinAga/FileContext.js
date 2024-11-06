96711859e30101273acec05b1b4b8854b354ebe5
rahul208in <rahul208in@gmail.com>
2024-11-06 18:23:02 +0800

    Create FileContext.js

---
+++ DinAga/FileContext.js
@@ -0,0 +1,24 @@
+
+import React, { createContext, useState, useContext } from 'react';
+
+const FileContext = createContext();
+
+export const FileProvider = ({ children }) => {
+  const [files, setFiles] = useState([]);
+
+  const addFile = (file) => {
+    setFiles((prevFiles) => [...prevFiles, file]);
+  };
+
+  const deleteFile = (fileName) => {
+    setFiles((prevFiles) => prevFiles.filter((file) => file.name !== fileName));
+  };
+
+  return (
+    <FileContext.Provider value={{ files, addFile, deleteFile }}>
+      {children}
+    </FileContext.Provider>
+  );
+};
+
+export const useFiles = () => useContext(FileContext);
