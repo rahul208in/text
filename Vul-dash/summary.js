ab7f42bf3edca2ec20bd6c300b6d9c627825279e
rahul208in <rahul208in@gmail.com>
2024-11-14 13:17:43 +0800

    Create summary.js

---
+++ Vul-dash/summary.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+const SummaryPage = () => {
+  return (
+    <div>
+      <h2 className="text-2xl font-semibold mb-4">Vulnerability Summary</h2>
+      {/* Placeholder for summary data */}
+      <p>High: 10</p>
+      <p>Medium: 15</p>
+      <p>Low: 5</p>
+      {/* More summary and filter components will go here */}
+    </div>
+  );
+};
+
+export default SummaryPage;
