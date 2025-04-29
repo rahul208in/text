5b7ccd73c65caf28f9dfdcf6e912a52291d466dd
rahul208in <rahul208in@gmail.com>
2025-04-30 00:04:04 +0800

    Create page.js

---
+++ SWAGGERV1/page.js
@@ -0,0 +1,12 @@
+"use client";
+import React from 'react';
+import APIExplorer from './components/APIExplorer';
+import './globals.css';
+
+export default function Home() {
+  return (
+    <main>
+      <APIExplorer />
+    </main>
+  );
+}
